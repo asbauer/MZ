430231ee6f08c8a7c5ad9a494552d007d1536518
Adam Bauer <asbauer@ucsd.edu>
2025-03-18 16:23:01 -0700

    Moved requirements into backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -162,11 +162,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
+#CORS_ALLOWED_ORIGINS = [
       # React frontend URL
     # Add other allowed origins if needed
-    'http://localhost:5173'
-]
+#    'http://localhost:5173',
+    
+#]
 
 
 CORS_ALLOWS_CREDENTIALS = True
