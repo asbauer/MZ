437c0a5b3bdc10793bde4b4f5119b7a869c5090d
Adam Bauer <asbauer@ucsd.edu>
2025-03-25 01:04:56 -0700

    Changed forgot password temporarily

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -20,34 +20,14 @@ const handleSubmit = (e) => {
 
 
     return <>
-    <h1 style={{textAlign:"center", marginBottom:'2px' }}>Reset Password</h1>
-    <h2 style={{textAlign:"center"}}>Enter your email to reset your password.</h2>
-    <form onSubmit={handleSubmit}>
-        <input 
-        disabled={submitted}
-        id='email-reset-field'
-        type="email"
-        placeholder="Enter your email here"
-        onChange={(e) => setEmail(e.target.value) }
-         />
-         { message ? 
-         <div style={{display:"flex",flexDirection:'column' }}>
-          <p style={{textAlign: 'center', color: 'blue', marginTop: '10px'}}>
-                    {message}
-                </p>
-                <button className="enlarge-on-hover"  style={{margin:'auto'}} onClick={() => navigate('/login/')}>
+
+        <h2 style={{textAlign:'center'}}>Just ask your son to help you! He'll take care of it! </h2>
+   
+                <button className="enlarge-on-hover"  style={{margin:'auto', borderRadius:'8px'}} onClick={() => navigate('/login/')}>
                     Login
                 </button>
                 
-                </div>
-         :
-         <button className="enlarge-on-hover" type="submit">Reset Password</button>
-
-        }
-    </form>
-   
-    
-    
+        
     </>
 }
 
